5f9fb1c54616089ebe5020148588f2698286e24b
Zero <zeroto521@gmail.com>
2021-01-03 17:50:30 +0800

    :tada: finish 7.8

--- src/App.js
+++ src/App.js
@@ -1,74 +1,51 @@
-  
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
 
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
-  }
-
-  const service = {
-    create
-  }
-
-  return [
-    resources, service
-  ]
-}
+import { useField, useResource } from './hooks'
+import AddNote from './components/AddNote'
+import AddPerson from './components/AddPerson'
+import Notes from './components/Notes'
+import Persons from './components/Persons'
 
 const App = () => {
   const content = useField('text')
   const name = useField('text')
   const number = useField('text')
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+  const baseUrl = 'http://localhost:3005'
 
-  const handleNoteSubmit = (event) => {
+  const [notes, noteService] = useResource(`${baseUrl}/notes`)
+  const [persons, personService] = useResource(`${baseUrl}/persons`)
+
+  useEffect(() => {
+    noteService.getAll()
+    personService.getAll()
+  }, [])
+
+  function handleNoteSubmit(event) {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({
+      'name': name.value,
+      'number': number.value
+    })
   }
 
   return (
     <div>
-      <h2>notes</h2>
-      <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
-        <button>create</button>
-      </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
-
-      <h2>persons</h2>
-      <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
-        <button>create</button>
-      </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      <div>
+        <h2>notes</h2>
+        <AddNote handleSubmit={handleNoteSubmit} content={content} />
+        <Notes notes={notes} />
+      </div>
+      <div>
+        <h2>persons</h2>
+        <AddPerson handleSubmit={handlePersonSubmit} name={name} number={number} />
+        <Persons persons={persons} />
+      </div>
     </div>
   )
 }
